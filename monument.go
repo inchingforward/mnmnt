31045c16095de5f585c9b4b8958060be2a19ebb5
inchingforward <inchingforward@gmail.com>
2016-07-30 10:55:30 -0500

    Added approval email.

--- monument.go
+++ monument.go
@@ -15,6 +15,7 @@ import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/engine/standard"
 	_ "github.com/lib/pq"
+	"github.com/mailgun/mailgun-go"
 	"github.com/russross/blackfriday"
 	"github.com/satori/go.uuid"
 )
@@ -115,10 +116,34 @@ func createMemory(c echo.Context) error {
 	_, err := db.NamedExec("insert into memory values (default, :title, :details, :latitude, :longitude, :author, false, :approval_uuid, now(), now())", m)
 	if err != nil {
 		return render(c, "memory.html", m, err)
-	} else {
-		// FIXME:  send approval email
-		return render(c, "memory_submitted.html", m, err)
 	}
+
+	sendEmail(m)
+
+	return render(c, "memory_submitted.html", m, err)
+}
+
+func sendEmail(memory *Memory) {
+	domain := os.Getenv("MONUMENT_MAILGUN_DOMAIN")
+	prvKey := os.Getenv("MONUMENT_MAILGUN_PRIVATE_KEY")
+	pubKey := os.Getenv("MONUMENT_MAILGUN_PUBLIC_KEY")
+	address := os.Getenv("MONUMENT_ADMIN_EMAIL")
+	mnmntHost := os.Getenv("MONUMENT_HOST")
+
+	if domain == "" || prvKey == "" || pubKey == "" || address == "" || mnmntHost == "" {
+		log.Println("Missing mail environment variables...not sending")
+		return
+	}
+
+	approvalLink := fmt.Sprintf("%v/memories/approve/%v", mnmntHost, memory.ApprovalUuid)
+	subject := "New Monument memory submitted"
+	body := fmt.Sprintf("%v:\n\n%v\n\n-%v\n\nApproval link: %v", memory.Title, memory.Details, memory.Author, approvalLink)
+
+	gun := mailgun.NewMailgun(domain, prvKey, pubKey)
+	msg := mailgun.NewMessage(address, subject, body, address)
+
+	response, id, err := gun.Send(msg)
+	log.Printf("mailer response: %v, message: %v, error: %v\n", id, response, err)
 }
 
 func getMemorySubmitted(c echo.Context) error {
