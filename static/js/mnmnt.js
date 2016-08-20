1755917d34b21462f8a4260710c3d4e5cd6ab9c9
inchingforward <inchingforward@gmail.com>
2016-08-20 11:44:22 -0500

    Added maps styles.

--- static/js/mnmnt.js
+++ static/js/mnmnt.js
@@ -1,3 +1,5 @@
+var MAP_STYLES = [{"featureType":"landscape","stylers":[{"hue":"#FFBB00"},{"saturation":43.400000000000006},{"lightness":37.599999999999994},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#FFC200"},{"saturation":-61.8},{"lightness":45.599999999999994},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":51.19999999999999},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":52},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#0078FF"},{"saturation":-13.200000000000003},{"lightness":2.4000000000000057},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#00FF6A"},{"saturation":-1.0989010989011234},{"lightness":11.200000000000017},{"gamma":1}]}];
+
 var RecentMemories = RecentMemories || (function() {
     var infoWindow, map, bounds, markerAndContent;
     
@@ -10,7 +12,8 @@ var RecentMemories = RecentMemories || (function() {
             scaleControl: false,
             streetViewControl: false,
             rotateControl: false,
-            fullscreenControl: false
+            fullscreenControl: false,
+            styles: MAP_STYLES
         };
 
         infoWindow = new google.maps.InfoWindow();
@@ -82,7 +85,8 @@ var AddMemory = AddMemory || (function() {
             scaleControl: false,
             streetViewControl: false,
             rotateControl: false,
-            fullscreenControl: false
+            fullscreenControl: false,
+            styles: MAP_STYLES
         };
 
         map = new google.maps.Map(document.getElementById('address_search_map'), mapOptions);
@@ -183,7 +187,8 @@ var MemoryDetails = MemoryDetails || (function() {
             scaleControl: false,
             streetViewControl: false,
             rotateControl: false,
-            fullscreenControl: false
+            fullscreenControl: false,
+            styles: MAP_STYLES
         };
 
         var map = new google.maps.Map(document.getElementById("memory_map"), mapOptions);
